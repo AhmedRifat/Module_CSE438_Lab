2f95f412bcd35a721a6f9294e386b29490517c40
Redown <redowanahmedrifat@gmail.com>
2025-02-23 22:20:27 +0600

    For importing code

---
+++ Module/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='Module',
+      version='0.1',
+      description='Utilities for lab exercises',
+      url='https://github.com/AhmedRifat/Module_CSE438_Lab',
+      packages=find_packages(),
+      zip_safe=True)
